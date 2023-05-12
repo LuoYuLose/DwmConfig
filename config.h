87859af9a178ccb7c5d64e007b9a872c97f16eb8
LuoYuLose <2843966358@qq.com>
2023-05-12 14:37:18 +0800

    改个字体

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=15" };
+static const char *fonts[]          = { "Source Code Pro:size=18" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#D8DEE9";
 static const char col_gray2[]       = "#D8DEE9";
